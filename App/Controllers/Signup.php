425cfebeea33ffd971d495706cb0cc761e5eb5ee
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-04-07 22:39:21 -0400

    added user model with a save method to DB

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use \Core\View;
+use \App\Models\User;
 
 /**
  * Signup controller
@@ -28,6 +29,10 @@ class Signup extends \Core\Controller
    */
   public function createAction()
   {
-    var_dump($_POST);
+    $user = new User($_POST);
+
+    $user->save();
+
+    View::renderTemplate('Signup/success.html');
   }
 }
