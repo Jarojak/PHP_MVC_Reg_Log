506873f3de8832f22cc728800c988d168f43eeb6
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-04-12 20:28:16 -0400

    add flash message types and give them different styles

--- App/Flash.php
+++ App/Flash.php
@@ -12,13 +12,32 @@ class Flash
 {
 
     /**
+     * Success message type
+     * @var string
+     */
+    const SUCCESS = 'success';
+
+    /**
+     * Information message type
+     * @var string
+     */
+    const INFO = 'info';
+
+    /**
+     * Warning message type
+     * @var string
+     */
+    const WARNING = 'warning';
+
+    /**
      * Add a message
      *
      * @param string $message  The message content
+     * @param string $type  The optional message type, defaults to SUCCESS
      *
      * @return void
      */
-    public static function addMessage($message)
+    public static function addMessage($message, $type = 'success')
     {
         // Create array in the session if it doesn't already exist
         if (! isset($_SESSION['flash_notifications'])) {
@@ -26,7 +45,10 @@ class Flash
         }
 
         // Append the message to the array
-        $_SESSION['flash_notifications'][] = $message;
+        $_SESSION['flash_notifications'][] = [
+            'body' => $message,
+            'type' => $type
+        ];
     }
 
     /**
@@ -37,6 +59,7 @@ class Flash
     public static function getMessages()
     {
         if (isset($_SESSION['flash_notifications'])) {
+            //return $_SESSION['flash_notifications'];
             $messages = $_SESSION['flash_notifications'];
             unset($_SESSION['flash_notifications']);
 
