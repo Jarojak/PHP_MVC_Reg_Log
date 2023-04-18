82b0ea69a1ae2b4eaa7bacb5f2f116a50c61af8c
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-04-17 22:18:54 -0400

    user profile: let the user update their own details

---
+++ App/Controllers/Profile.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+/**
+ * Profile controller
+ *
+ * PHP version 7.0
+ */
+class Profile extends Authenticated
+{
+
+    /**
+     * Show the profile
+     *
+     * @return void
+     */
+    public function showAction()
+    {
+        View::renderTemplate('Profile/show.html');
+    }
+}
